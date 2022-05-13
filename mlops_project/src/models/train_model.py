afadaebdd92e9f0ccf1e581efc0eaa9103eb306f
skovenforrest <tobiasskov95@gmail.com>
2022-05-13 15:49:34 +0200

    new way of making dataset with list

--- mlops_project/src/models/train_model.py
+++ mlops_project/src/models/train_model.py
@@ -10,30 +10,31 @@ from torchvision import datasets, transforms
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 import pytorch_lightning as pl
-
-
+from PIL import Image
+import pickle
+import os
 log = logging.getLogger(__name__)
 
 
 
 
 class AnimalDataset(Dataset):
-    def __init__(self, labels, images):
+    def __init__(self, labels, images_names,dir):
         self.img_labels = labels
-        self.img = images
+        self.image_names = images_names
+        self.dir = dir
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        image =self.img[idx]
+        image = Image.open(os.path.join(self.dir,self.image_names[idx]))
         image = transform_images(image)
         label = self.img_labels[idx]   
         return image, label
 
-
 def transform_images(data):
-    transform = transforms.Compose([transforms.Resize((128,128))])
+    transform = transforms.Compose([transforms.Resize((128,128)),transforms.ToTensor(),transforms.Normalize((0.5320, 0.5095, 0.4346), (0.2765, 0.2734, 0.2861))])
     return transform(data)
 
 @hydra.main(config_path="config", config_name='default_config.yaml')
@@ -50,14 +51,28 @@ def train(config):
     #args = parser.parse_args(sys.argv[2:])
     #print(args)
     dir = "C:\\Users\\Tobias\\Documents\\DTU\mlops_project\\mlops_project\\data\\processed\\"
-    train_images = torch.load(dir + "train_images.pt")
-    train_labels = torch.load(dir + "train_labels.pt")
-    test_images = torch.load(dir + "test_images.pt")
-    test_labels = torch.load(dir + "test_labels.pt")
+    dir_dataloader = "C:\\Users\\Tobias\\Documents\\DTU\mlops_project\\mlops_project"
+
+    with open(dir+"test_img_list", "rb") as fp:   # Unpickling
+        test_img_list = pickle.load(fp)
+
+    with open(dir+"train_img_list", "rb") as fp:   # Unpickling
+        train_img_list = pickle.load(fp)
+
+    with open(dir+"test_targets", "rb") as fp:   # Unpickling
+        test_targets = pickle.load(fp)
+
+    with open(dir+"train_targets", "rb") as fp:   # Unpickling
+        train_targets = pickle.load(fp)
+
+    #train_images = torch.load(dir + "train_images.pt")
+    #train_labels = torch.load(dir + "train_labels.pt")
+    #test_images = torch.load(dir + "test_images.pt")
+    #test_labels = torch.load(dir + "test_labels.pt")
 
 
-    train_dataset = AnimalDataset(train_labels,train_images)
-    test_dataset  = AnimalDataset(test_labels,test_images)
+    train_dataset = AnimalDataset(train_targets,train_img_list,dir_dataloader)
+    test_dataset  = AnimalDataset(test_targets,test_img_list,dir_dataloader)
 
     train_dataloader = DataLoader(train_dataset, batch_size=hparams["batch_size"], shuffle=True, num_workers=6)
     test_dataloader = DataLoader(test_dataset, batch_size=hparams["batch_size"], shuffle= False, num_workers=6)
@@ -84,32 +99,6 @@ def train(config):
     torch.save(model.state_dict(), "models/baseline.pth")
 
     log.info("Finish!!")
-
-def test_score(test_set,model,criterion):
-    
-    running_loss = 0.0
-    accuracy = 0.0
-    total = 0
-    correct = 0
-    with torch.no_grad():
-
-        for i, data in enumerate(test_set, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-
-            # forward + backward + optimize
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-
-            classes = torch.argmax(outputs, 1)
-            total += labels.size(0)
-            correct += (classes == labels).sum().item()
-            running_loss += loss.item()
-
-        accuracy = 100.*correct/total
-        test_loss = running_loss/len(test_set)
-    return accuracy, test_loss
-
 
 if __name__ == '__main__':
     train()
