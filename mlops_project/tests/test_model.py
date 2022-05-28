b9fe0f7b9a071d1ac8358a0be70c20bdf6ab83f3
skovenforrest <tobiasskov95@gmail.com>
2022-05-28 13:42:10 +0200

    added tests and github tests

---
+++ mlops_project/tests/test_model.py
@@ -0,0 +1,11 @@
+import pytest
+import torch
+from src.models.model import MyAwesomeModel
+
+class Test_model:
+
+    @pytest.mark.parametrize("input,expected_output", [(torch.rand((64, 3, 128,128)), torch.Size([64, 10])), (torch.rand((32, 3, 128,128)), torch.Size([32, 10]))])
+    def test_output_shape(self, input, expected_output):
+        model =  MyAwesomeModel()
+        output = model(input)
+        assert output.shape == expected_output
